5ef593da65c2dcaae66b77a8c3fb29267b4652cf
Ryota Konabe <rt.konabe@gmail.com>
2020-12-09 22:53:31 +0900

    [add] ganacheの接続設定を適用

--- truffle-config.js
+++ truffle-config.js
@@ -3,5 +3,11 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "vapp/src/contracts"),
-
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545, // ganache default
+      network_id: "*" // Match any network id
+    }
+  }
 };
